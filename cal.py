b9d0b663f669af897a0d8c64421450e65a684fab
gtsofa <jlsnyule@yahoo.com>
2018-02-28 22:24:45 +0300

    add files

---
+++ cal.py
@@ -0,0 +1,17 @@
+# calculator class that will perform test on
+
+def add(x, y):
+    """add two numbers"""
+    return x + y
+
+def subtract(x, y):
+    """subtract two numbers"""
+    return x - y
+
+def multiply(x, y):
+    """multiply two numbers"""
+    return x * y
+
+def divide(x, y):
+    """divide two numbers"""
+    return x / y
