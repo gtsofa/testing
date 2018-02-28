3e296fc67b20f3aa4e0fd3e57d38f4ce3b6ad67d
gtsofa <jlsnyule@yahoo.com>
2018-03-01 00:02:06 +0300

    refactor

--- test_employee.py
+++ test_employee.py
@@ -6,43 +6,47 @@ from employee import Employee
 
 class TestEmployee(unittest.TestCase):
 
-    def test_email(self):
-        emp_1 = Employee('John', 'Doe', 520000)
-        emp_2 = Employee('Tsofa', 'Nyule', 700000)
+    # DRY
+    def setUp(self):
+        """will run before any test method"""
+        self.emp_1 = Employee('John', 'Doe', 520000)
+        self.emp_2 = Employee('Tsofa', 'Nyule', 700000)
 
-        self.assertEqual(emp_1.email, 'John.Doe@email.com')
-        self.assertEqual(emp_2.email, 'Tsofa.Nyule@email.com')
+    def tearDown(self):
+        """will run after test method"""
+
+        pass
+
+    def test_email(self):
+
+        self.assertEqual(self.emp_1.email, 'John.Doe@email.com')
+        self.assertEqual(self.emp_2.email, 'Tsofa.Nyule@email.com')
 
         emp_1.first = 'Julis'
         emp_2.first = 'Samuel'
 
-        self.assertEqual(emp_1.email, 'Julis.Doe@email.com')
-        self.assertEqual(emp_2.email, 'Samuel.Nyule@email.com')
+        self.assertEqual(self.emp_1.email, 'Julis.Doe@email.com')
+        self.assertEqual(self.emp_2.email, 'Samuel.Nyule@email.com')
 
     def test_fullname(self):
-        emp_1 = Employee('John', 'Doe', 520000)
-        emp_2 = Employee('Tsofa', 'Nyule', 700000)
 
-        self.assertEqual(emp_1.fullname, 'John Doe')
-        self.assertEqual(emp_2.fullname, 'Tsofa Nyule')
+        self.assertEqual(self.emp_1.fullname, 'John Doe')
+        self.assertEqual(self.emp_2.fullname, 'Tsofa Nyule')
 
-        emp_1.first = 'Julis'
-        emp_2.first = 'Samuel'
+        self.emp_1.first = 'Julis'
+        self.emp_2.first = 'Samuel'
 
-        self.assertEqual(emp_1.fullname, 'Julis Doe')
-        self.assertEqual(emp_2.fullname, 'Samuel Nyule')
+        self.assertEqual(self.emp_1.fullname, 'Julis Doe')
+        self.assertEqual(self.emp_2.fullname, 'Samuel Nyule')
 
     def test_apply_raise(self):
         #check the apply raise funct
 
-        emp_1 = Employee('John', 'Doe', 520000)
-        emp_2 = Employee('Tsofa', 'Nyule', 700000)
-
         emp_1.apply_raise()
         emp_2.apply_raise()
         
-        self.assertEqual(emp_1.pay, 546000)
-        self.assertEqual(emp_2.pay, 735000)
+        self.assertEqual(self.emp_1.pay, 546000)
+        self.assertEqual(self.emp_2.pay, 735000)
 
 if __name__=='__main__':
     unittest.main()
