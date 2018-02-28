02fa18dec7d4240946063cdb5695ede74a85421d
gtsofa <jlsnyule@yahoo.com>
2018-02-28 23:09:57 +0300

    refactor

--- cal.py
+++ cal.py
@@ -14,4 +14,7 @@ def multiply(x, y):
 
 def divide(x, y):
     """Divide Function"""
+    if y == 0:
+        raise ValueError('Can not divide by zero!')
     return x / y
+
--- test_cal.py
+++ test_cal.py
@@ -9,7 +9,35 @@ class TestCalc(unittest.TestCase):
     def test_add(self):
         """Test add function"""
         #result = cal.add(10,5)
-        self.assertEqual(cal.add(10,5), 15)
+        self.assertEqual(cal.add(10,5), 15)#hcases
+        self.assertEqual(cal.add(-1,1), 0)
+        self.assertEqual(cal.add(-1,-1), -2)
+
+    def test_subtract(self):
+        """Test add function"""
+        #result = cal.add(10,5)
+        self.assertEqual(cal.subtract(10,5), 5)#hcases
+        self.assertEqual(cal.subtract(-1,1), -2)
+        self.assertEqual(cal.subtract(-1,-1), 0)
+
+    def test_multiply(self):
+        """Test add function"""
+        #result = cal.add(10,5)
+        self.assertEqual(cal.multiply(10,5), 50)#hcases
+        self.assertEqual(cal.multiply(-1,1), -1)
+        self.assertEqual(cal.multiply(-1,-1), 1)
+
+    def test_divide(self):
+        """Test add function"""
+        #result = cal.add(10,5)
+        self.assertEqual(cal.divide(10,5), 2)#hcases
+        self.assertEqual(cal.divide(-1,1), -1)
+        self.assertEqual(cal.divide(-1,-1), 1)
+
+        #self.assertRaises(ValueError, cal.divide, 10, 2)#3 args -exception we expect,fun we r testing,arguments for the functio
+        #using the contextmanager
+        with self.assertRaises(ValueError):
+            cal.divide(10, 0)
 
 if __name__=="__main__":
     unittest.main()
